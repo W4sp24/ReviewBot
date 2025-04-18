45079dff46472af20880fc0f68c27be484b9d0c2
W4sp24 <ethansoliven1@gmail.com>
2025-04-19 02:26:37 +0800

    Promts Class

---
+++ src/__pycache__/promts.py
@@ -0,0 +1,7 @@
+
+class Promts:
+    def __init__(self):
+        self.name = "Gemini"  # Initialize name
+        self.symbol = ""      # Initialize symbol
+        self.client = None    # Initialize client
+        self.model = None     # Initialize model
